5b02b01873bafb19650a793ccc49612c39feb236
Jihyun247 <wlguslove12345@gmail.com>
2022-10-19 14:39:05 +0900

    FEAT: room test view & viewcontroller

---
+++ Projects/FeatureModule/Sources/Jihyeon/RoomTestView.swift
@@ -0,0 +1,23 @@
+//
+//  RoomTestView.swift
+//  FeatureModuleTests
+//
+//  Created by 김지현 on 2022/10/19.
+//  Copyright © 2022 turnaround.io. All rights reserved.
+//
+
+import UIKit
+
+class RoomTestView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    var roomView =
+}
---
+++ Projects/FeatureModule/Sources/Jihyeon/RoomTestViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RoomTestViewController.swift
+//  FeatureModuleTests
+//
+//  Created by 김지현 on 2022/10/19.
+//  Copyright © 2022 turnaround.io. All rights reserved.
+//
+
+import Foundation
